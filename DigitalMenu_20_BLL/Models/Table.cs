5bdfa5363e7cd6abd3109df534604fb623e24a6b
Jens Ramakers <jensramakers@gmail.com>
2024-03-04 15:41:36 +0100

    tested database connection

--- DigitalMenu_20_BLL/Models/Table.cs
+++ DigitalMenu_20_BLL/Models/Table.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace DigitalMenu_20_BLL.Models;
 
-namespace DigitalMenu_20_BLL.Models
+public class Table
 {
-    public class Table
-    {
-        public int Id { get; set; }
-        public long QRCode { get; set; }
-        public List<Order> Orders { get; set; }
-    }
+    public int Id { get; set; }
+
+    public string QRCode { get; set; }
+
+    public List<Order> Orders { get; set; }
 }
