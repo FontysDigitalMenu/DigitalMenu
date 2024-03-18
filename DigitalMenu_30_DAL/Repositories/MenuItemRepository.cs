b0d86665e0b99eab35a076f42133cc6c619e6cb8
Hu,Tony T.C. <tony.hu@student.fontys.nl>
2024-03-18 15:13:59 +0100

    added other layers for the get categories with menuItems per page

--- DigitalMenu_30_DAL/Repositories/MenuItemRepository.cs
+++ DigitalMenu_30_DAL/Repositories/MenuItemRepository.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using DigitalMenu_20_BLL.Interfaces.Repositories;
 using DigitalMenu_20_BLL.Models;
 using DigitalMenu_30_DAL.Data;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace DigitalMenu_30_DAL.Repositories
@@ -25,6 +26,27 @@ namespace DigitalMenu_30_DAL.Repositories
                 .Where(m => m.Id > lastId)
                 .Take(amount)
                 .ToList();
+        }
+
+        public IEnumerable<MenuItem> GetNextMenuItemsWithCategory(int lastId, int amount)
+        {
+            return _dbContext.MenuItems
+                .Include(m => m.Categories)
+                .OrderBy(m => m.Id)
+                .Where(m => m.Id > lastId)
+                .Take(amount)
+                .ToList();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _dbContext.Categories
+                .ToList();
+        }
+
+        public MenuItem GetMenuItemBy(int id)
+        {
+            return _dbContext.MenuItems.Find(id);  
         }
     }
 }
