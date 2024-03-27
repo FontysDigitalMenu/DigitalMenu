8f2d9a984fbd32ae8fa5543d19e7b846b6c84974
jens <jensramakers@gmail.com>
2024-03-27 10:41:55 +0100

    added all tests for TableServiceTests.cs

--- UnitTests/Services/TableServiceTests.cs
+++ UnitTests/Services/TableServiceTests.cs
@@ -34,4 +34,64 @@ public class TableServiceTests
         // Assert
         Assert.That(result, Is.EqualTo(tables));
     }
+    
+    [Test]
+    public void Create_ShouldReturnCreatedTable()
+    {
+        // Arrange
+        Table table = new() { Id = "CA3D0ED8-78D6-4690-8952-89D7E1FC18A4", Name = "Table 1", CreatedAt = DateTime.Now };
+        _tableRepositoryMock.Setup(x => x.Create(table))
+            .Returns(table);
+
+        // Act
+        Table? result = _tableService.Create(table);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(table));
+    }
+    
+    [Test]
+    public void GetById_ShouldReturnTableById()
+    {
+        // Arrange
+        Table table = new() { Id = "CA3D0ED8-78D6-4690-8952-89D7E1FC18A4", Name = "Table 1", CreatedAt = DateTime.Now };
+        _tableRepositoryMock.Setup(x => x.GetById(table.Id))
+            .Returns(table);
+
+        // Act
+        Table? result = _tableService.GetById(table.Id);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(table));
+    }
+    
+    [Test]
+    public void Update_ShouldReturnTrue()
+    {
+        // Arrange
+        Table table = new() { Id = "CA3D0ED8-78D6-4690-8952-89D7E1FC18A4", Name = "Table 1", CreatedAt = DateTime.Now };
+        _tableRepositoryMock.Setup(x => x.Update(table))
+            .Returns(true);
+
+        // Act
+        bool result = _tableService.Update(table);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
+    
+    [Test]
+    public void Delete_ShouldReturnTrue()
+    {
+        // Arrange
+        const string id = "CA3D0ED8-78D6-4690-8952-89D7E1FC18A4";
+        _tableRepositoryMock.Setup(x => x.Delete(id))
+            .Returns(true);
+
+        // Act
+        bool result = _tableService.Delete(id);
+
+        // Assert
+        Assert.That(result, Is.True);
+    }
 }
