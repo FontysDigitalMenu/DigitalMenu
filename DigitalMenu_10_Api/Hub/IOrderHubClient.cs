080a5b237803be40eccf2629195a4b81c18fb6d8
jens <jensramakers@gmail.com>
2024-05-14 12:04:15 +0200

    added drinks update

--- DigitalMenu_10_Api/Hub/IOrderHubClient.cs
+++ DigitalMenu_10_Api/Hub/IOrderHubClient.cs
@@ -5,6 +5,8 @@ namespace DigitalMenu_10_Api.Hub;
 public interface IOrderHubClient
 {
     Task ReceiveOrder(OrderViewModel orderViewModel);
+    
+    Task ReceiveOrderDrinksUpdate();
 
     Task ReceiveOrderUpdate(OrderViewModel orderViewModel);
 
