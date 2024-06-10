b21598a54a089afb063af4f6d85b66be0280a1f7
Hu,Tony T.C. <tony.hu@student.fontys.nl>
2024-06-10 13:26:31 +0200

    added logic for drinks progressbar

--- DigitalMenu_30_DAL/Repositories/OrderRepository.cs
+++ DigitalMenu_30_DAL/Repositories/OrderRepository.cs
@@ -20,6 +20,8 @@ public class OrderRepository(ApplicationDbContext dbContext) : IOrderRepository
             .Include(o => o.Splits)
             .Include(o => o.OrderMenuItems)
             .ThenInclude(omi => omi.MenuItem)
+            .ThenInclude(mi => mi.CategoryMenuItems)
+            .ThenInclude(cmi => cmi.Category)
             .FirstOrDefault(o => o.Id == id && o.SessionId == tableSessionId);
     }
 
